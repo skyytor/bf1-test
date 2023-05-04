cc23e0d89a9ec62e855f681d4ed8d4162deedee3
天骏 王 <skyytor@outlook.com>
2023-05-04 20:33:07 +0800

    路由配合玩家列表初步完成

--- bf1-test/src/views/scoreView.vue
+++ bf1-test/src/views/scoreView.vue
@@ -1,9 +1,32 @@
 <script setup>
+import axios from 'axios'
+import { ref,reactive } from 'vue';
+
+
+const score123 = reactive([])
+
+console.log(score123);
+function GetPlayerList() {
+  axios({
+    method:'get',
+    url: 'http://localhost:5051/Player/GetPlayerList',
+  }).then(function (scores){
+    score123.value=scores.data;
+    console.log(scores);
+  })
+}
 
 </script>
 
 <template>
-  <div class="details">
-    <h1>This is an details page</h1>
-  </div>
+  <h1>Hello World</h1>
+  <button @click="GetPlayerList">获取玩家列表</button>
+
+  <ul>
+  <li v-for="score in score123.value">
+    {{ score.rank }}  {{ score.name }}####{{ score.kill }} / {{ score.dead }} {{ score.weaponS0 }}
+  </li>
+</ul>
 </template>
+<style scoped>
+</style>
