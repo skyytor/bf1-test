e6fa7b76218e083b32e5717d488445c77a107dd4
天骏 王 <skyytor@outlook.com>
2023-05-04 16:27:08 +0800

    不知道使啥

--- bf1-test/src/App.vue
+++ bf1-test/src/App.vue
@@ -3,27 +3,60 @@
 </script>
 
 <template>
-    
-        <div class="theme">
-            <router-link to="/home">主页</router-link>
-            <router-link to="/power">权限</router-link>
-            <router-link to="/rules">规则</router-link>
-            <router-link to="/monitor">监控</router-link>
-            <router-link to="/chat">聊天</router-link>
-            <router-link to="/about">关于</router-link>
+    <div class="whole">
 
+
+        <div class="aside">
+                <router-link to="/home">主页</router-link>
+                <router-link to="/power">权限</router-link>
+                <router-link to="/rules">规则</router-link>
+                <router-link to="/monitor">监控</router-link>
+                <router-link to="/chat">聊天</router-link>
+                <router-link to="/about">关于</router-link>
         </div>
-    
-    <main>
+
+
         <div class="container">
-            <router-view></router-view>
+            <main>
+                <div class="container">
+                    <router-view></router-view>
+                </div>
+            </main>
         </div>
-    </main>
+
+    </div>
 </template>
 
 <style>
+.whole{
+    width: 100%;
+  height: 100vh;
+  display: flex;
+  background-color: aliceblue;
+}
+.aside{
+    border: 2px ridge rgb(248, 185, 185);
+    background-color: rgb(245, 255, 241);
+    padding: 0.5rem;
+    display: flex;
+    flex-direction: column;
+    flex: 0 1 20%;
+    overflow-y: auto;
+    height: 95vh;
+    
+}
+/* .theme {
+    border: 2px ridge rgb(248, 185, 185);
+    background-color: rgb(245, 255, 241);
+    padding: 0.5rem;
+    display: flex;
+    flex-direction: column;
+    flex: 0 1 20%;
+    overflow-y: auto;
+    height: 95vh;
+} */
 
-.container{
+.main {
     flex-wrap: nowrap;
 }
 </style>
